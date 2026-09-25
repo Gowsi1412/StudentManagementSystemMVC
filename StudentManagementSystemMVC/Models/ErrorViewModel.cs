25ce71b6730f67cc4276097e99ff436b09416309
Gowsi1412 <gowsalyap1412@gmail.com>
2026-09-25 10:42:49 +0530

    Add project files.

---
+++ StudentManagementSystemMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StudentManagementSystemMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
